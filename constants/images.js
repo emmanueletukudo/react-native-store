893bd4059e4a5717e8ae53b7c7c39c898bf0085b
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-03-06 17:08:05 +0100

    added new image to image constant

--- constants/images.js
+++ constants/images.js
@@ -7,6 +7,7 @@ export const shirt2 = require("../assets/images/shirt2.png");
 export const shoe1 = require("../assets/images/shoe1.png");
 export const sweater3 = require("../assets/images/sweater3.png");
 export const sweater5 = require("../assets/images/sweater5.png");
+export const searches = require("../assets/images/recent-searches.png");
 
 
 export default{
@@ -19,4 +20,5 @@ export default{
     shoe1,
     sweater3,
     sweater5,
+    searches,
 }
