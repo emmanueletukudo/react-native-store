4b7ccd5b210550a734f198e690c44c5e97bc95ee
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-03-06 16:32:51 +0100

    created recent searches container

--- screens/Home.js
+++ screens/Home.js
@@ -34,6 +34,117 @@ const Home = () => {
         }
     ])
 
+    const [recentSearches, setRecentSearch] = React.useState([
+        {
+            id: 0,
+            name: "Jacket 4",
+            img: images.jacket4,
+            bgColor: "#D09040",
+            price: "$250",
+            type: "Featured",
+            sizes: [6, 7, 8, 9, 10, 16],
+        },
+        {
+            id: 1,
+            name: "Jacket 1",
+            img: images.jacket1,
+            bgColor: "#D3D1C8",
+            type: "Featured",
+            price: "$150" ,
+            sizes: [6, 7, 8, 9, 10, 12],
+        },
+        {
+            id: 2,
+            name: "Jacket 2",
+            img: images.jacket2,
+            type: "Featured",
+            bgColor: "#303946",
+            price: "$160" ,
+            sizes: [6, 7, 8, 9, 10],
+        },
+        {
+            id: 3,
+            name: "Hat 1",
+            img: images.hat1,
+            type: "Featured",
+            bgColor: "#303946",
+            price: "$100" ,
+            sizes: [6, 7, 8, 9, 10, 16],
+        },
+        {
+            id: 4,
+            name: "Shirt 1",
+            img: images.shirt1,
+            type: "Featured",
+            bgColor: "#303946",
+            price: "$100" ,
+            sizes: [6, 7, 8, 9, 10, 16],
+        },
+        {
+            id: 5,
+            name: "Shirt 2",
+            img: images.shirt2,
+            type: "Featured",
+            bgColor: "#303946",
+            price: "$100" ,
+            sizes: [6, 7, 8, 9, 10, 16],
+        },
+        {
+            id: 6,
+            name: "Shoe 1",
+            img: images.shoe1,
+            type: "Featured",
+            bgColor: "#303946",
+            price: "$100" ,
+            sizes: [6, 7, 8, 9, 10, 12],
+        },
+        {
+            id: 7,
+            name: "Sweater 3",
+            img: images.sweater3,
+            type: "Featured",
+            bgColor: "#303946",
+            price: "$100" ,
+            sizes: [6, 7, 8, 9, 10, 16, 18],
+        },
+        {
+            id: 8,
+            name: "Sweater 5",
+            img: images.sweater5,
+            type: "Featured",
+            bgColor: "#303946",
+            price: "$100" ,
+            sizes: [6, 7, 8, 9, 10, 18],
+        }
+    ])
+
+    function renderRecentSearches(item, index){
+        return(
+            <TouchableOpacity
+            style = {{flex: 1, flexDirection: "row"}}
+            onPress= {() => {console.log("renderRecentSearches")}}
+            >
+            <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+                <Image
+                source = {item.img}
+                resizeMode = "contain"
+                style = {{
+                    width: 130,
+                    height: 100,
+                }}
+                />
+            </View>
+            <View style={{
+                flex: 1.5,
+                marginLeft: SIZES.radius,
+                justifyContent: "center",
+            }}>
+                <Text>{item.name}</Text>
+                <Text>{item.price}</Text>
+            </View>
+            </TouchableOpacity>
+        )
+    }
     function renderFeaturedItems(item, index){
         return(
             <TouchableOpacity 
@@ -88,14 +199,22 @@ const Home = () => {
             {/* Recent Serches */}
             <View style={[{
                 flex: 1, 
+                flexDirection: "row",
                 marginTop: SIZES.padding, 
                 borderTopLeftRadius: 30, 
-                borderTopRightRadius: 30, backgroundColor: COLORS.white}, style.recentSearchShadow]
+                borderTopRightRadius: 30,
+                backgroundColor: COLORS.white}, 
+                style.recentSearchShadow]
                 }>
-                <View style={{width: 70, marginLeft: SIZES.base, backgroundColor: "#000"}}>
+                <View style={{width: 70, height: "100%", marginLeft: SIZES.base, backgroundColor: COLORS.lightGray}}>
                 </View>
-                <View>
-
+                <View style={{flex: 1, paddingBottom: SIZES.padding}}>
+                    <FlatList 
+                    showsVerticalScrollIndicator = {false}
+                    data = {recentSearches}
+                    keyExtractor = {item => item.id.toString()}
+                    renderItem = {({item, index}) => renderRecentSearches(item, index)}
+                    />
                 </View>
             </View>
         </View>
